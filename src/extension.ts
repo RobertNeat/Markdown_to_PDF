c3a384730301300445dc77add9d8c56d284b9159
Robert Sereda <robertgoogl@gmail.com>
2026-03-08 21:09:30 +0100

    feat:  sidebar card + startup script

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,18 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+class MarkdownToPdfProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
+	getTreeItem(element: vscode.TreeItem): vscode.TreeItem {
+		return element;
+	}
+
+	getChildren(): Thenable<vscode.TreeItem[]> {
+		return Promise.resolve([
+			new vscode.TreeItem('Convert Markdown to PDF', vscode.TreeItemCollapsibleState.None),
+		]);
+	}
+}
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -9,6 +21,12 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "markdown-to-pdf" is now active!');
+
+	// Register the sidebar view provider
+	const provider = new MarkdownToPdfProvider();
+	context.subscriptions.push(
+		vscode.window.registerTreeDataProvider('markdown-to-pdf-view', provider)
+	);
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
